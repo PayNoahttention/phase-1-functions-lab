a7ff222437c2c466399fa8d410e48c1728d34433
NoahSolis <NSolis1993@gmail.com>
2022-02-23 22:31:41 -0600

    commit tree 1dfee15d38220a1572b42ffddc90b311179e693f

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) {
+        return blocks - 42;
+    } else {
+        return 42 - blocks;
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+        return (destination - start) * 264;
+    } else {
+        return (start - destination) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let numberOfBlocks = Math.abs(destination - start);
+    let totalDistance = numberOfBlocks * 264;
+
+    if (totalDistance < 400) {
+        return 0;
+    } else if (totalDistance > 400 && totalDistance <= 2000) {
+            totalDistance = totalDistance - 400;
+            return totalDistance * .02;
+    } else if (totalDistance >= 2001 && totalDistance <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far"
+    }
+}
